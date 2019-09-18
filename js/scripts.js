85633a09ccac6f534ff99f049876e52cd016ef78
David Boedigheimer <stealth259@gmail.com>
2019-09-18 09:39:09 -0700

    Add complete message and button to switch back and forth.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $("form#info").submit(function(event) {
     event.preventDefault();
+    $("#info").toggle();
+    $("#complete").toggle();
+  });
 
+  $("#new").click(function() {
+    $("#complete").toggle();
+    $("#info").toggle();
   });
 });
